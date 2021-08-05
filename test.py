0406bcc74a8704f68f62803aafed8e5679edde35
wangjinjian <wangjinjian@sjtu.edu.cn>
2021-08-06 00:24:23 +0800

    wjx

---
+++ test.py
@@ -0,0 +1,134 @@
+import re
+
+import requests, json, time, random, selenium
+from lxml import etree
+
+session = requests.session()
+
+
+def dan(ktimes, jqnonce):
+    result = []
+    b = ktimes % 10
+    if b == 0:
+        b = 1
+    for char in list(jqnonce):
+        f = ord(char) ^ b
+        result.append(chr(f))
+    return ''.join(result)
+
+
+def get_jqParam(rn, start_time, id1):
+    """
+    使用 rn,starttime,activityId三个参数
+    :return:  jqParam的值
+    """
+    le = int(rn.split(".")[0])
+    state = 3597397
+    p1 = 2147483648
+    node = 2147483647
+    p2 = ~~int(le / p1)
+    p3 = 0
+    node1 = le & node
+    index = 3597397
+    p4 = p2 ^ 0
+    item = node1 ^ index
+    locaOffset = (p4 * p1) + item
+    starttime = int((time.mktime(time.strptime(start_time, '%Y/%m/%d %X'))) * 1000)
+    offset = int(starttime / 1000)
+    node = str(offset)
+    if (offset % 10) > 0:
+        node = node[::-1]
+    fn = int(node + "89123")
+    value = list(str(fn) + str(locaOffset))
+    a = "kgESOLJUbB2fCteoQdYmXvF8j9IZs3K0i6w75VcDnG14WAyaxNqPuRlpTHMrhz"
+    result = list(a)
+    id = len(result)
+    for i in range(0, len(value)):
+        try:
+            key = int(value[i])
+        except:
+            pass
+        value1 = result[key]
+        el = result[id - 1 - key]
+        result[key] = el
+        result[id - 1 - key] = value1
+    a = "".join(result)
+    preCloseCallbackResult = a
+    littleEndian = fn + locaOffset + int(id1)
+
+    def abcd3(littleEndian, preCloseCallbackResult):
+        while True:
+            if littleEndian - 62 < 0:
+                return preCloseCallbackResult[littleEndian]
+            else:
+                p1 = littleEndian % 62
+                p2 = int(littleEndian / 62)
+                return abcd3(p2, preCloseCallbackResult) + preCloseCallbackResult[p1]
+
+    jqparam = abcd3(littleEndian, preCloseCallbackResult)
+    item = 0
+    map = list(jqparam)
+    for i in range(0, len(jqparam)):
+        item = item + ord(map[i])
+    el = len(jqparam)
+    selected = item % el
+    el1 = []
+    for i in range(selected, el):
+        el1.append(map[i])
+    for i in range(0, selected):
+        el1.append(map[i])
+    jqparam = "".join(el1)
+    return jqparam
+
+
+headers = {}
+qtr = {}
+ans = ''
+with open('header', 'r') as f:
+    for s in f.readlines():
+        arr = s.split(':')
+        headers[arr[0].strip()] = arr[1].strip()
+url = 'https://ks.wjx.top/vm/wURtp3K.aspx'
+res = session.get(url, headers=headers)
+text = res.text
+html = etree.HTML(text)
+
+with open('qtr', 'r') as f:
+    for s in f.readlines():
+        arr = s.split(':')
+        qtr[arr[0].strip()] = arr[1].strip()
+activityId = re.search(r'(?<=activityId \=)\d+', text).group()
+jqnonce = re.search(r'.{8}-.{4}-.{4}-.{4}-.{12}', text).group()
+ktimes = random.randint(30, 60)
+shortid = 'wURtp3K'
+starttime = re.search(r'\d+?/\d+?/\d+?\s\d+?:\d{2}:\d{2}', text).group()
+rn = re.search(r'\d{9,10}\.\d{8}', text).group()
+qtr['shortid'] = shortid
+qtr['t'] = time_stamp = '{}{}'.format(int(time.time()), random.randint(100, 200))
+qtr['starttime'] = starttime
+qtr['ktimes'] = ktimes
+qtr['rn'] = rn
+qtr['jcn'] = dan(ktimes, '测试')
+qtr['jqpram'] = get_jqParam(rn, starttime, activityId)
+qtr['jqnonce'] = jqnonce
+qtr['jqsign'] = dan(ktimes, jqnonce)
+print(qtr)
+
+nodes = html.xpath('//div[@class="field ui-field-contain"]')
+dic = {}
+for node in nodes:
+    id = node.xpath('./@topic')[0]
+    if id == '1':
+        dic[id] = '测试2'
+    elif id == '2':
+        dic[id] = '134'
+    else:
+        dic[id] = '1'
+
+sortkey = sorted(int(a) for a in dic.keys())
+for num in sortkey:
+    ans += '{}${}'.format(num, dic[str(num)]) + '}'
+print(ans[:-1])
+urlpost = 'https://ks.wjx.top/joinnew/processjq.ashx'
+res = session.post(url=urlpost, params=qtr, data={'submitdata': ans[:-1]})
+print(res.text)
---
+++ test2.py
@@ -0,0 +1,94 @@
+import re
+
+import requests, json, time, random, selenium
+from lxml import etree
+
+
+def dan(ktimes, jqnonce):
+    result = []
+    b = ktimes % 10
+    if b == 0:
+        b = 1
+    for char in list(jqnonce):
+        f = ord(char) ^ b
+        result.append(chr(f))
+    return ''.join(result)
+
+
+def get_jqParam(rn, start_time, id1):
+    """
+    使用 rn,starttime,activityId三个参数
+    :return:  jqParam的值
+    """
+    le = int(rn.split(".")[0])
+    state = 3597397
+    p1 = 2147483648
+    node = 2147483647
+    p2 = ~~int(le / p1)
+    p3 = ~~int(state / p1)
+    node1 = le & node
+    index = state & node
+    p4 = p2 ^ p3
+    item = node1 ^ index
+    locaOffset = (p4 * p1) + item
+    starttime = int((time.mktime(time.strptime(start_time, '%Y/%m/%d %H:%M:%S'))) * 1000)
+    print(starttime)
+    # starttime=1628075946440
+    offset = int(starttime / 1000)
+    node = str(offset)
+    if (offset % 10) > 0:
+        node = node[::-1]
+    fn = int(node + "89123")
+    value = list(str(fn) + str(locaOffset))
+    a = "kgESOLJUbB2fCteoQdYmXvF8j9IZs3K0i6w75VcDnG14WAyaxNqPuRlpTHMrhz"
+    result = list(a)
+    id = len(result)
+    for i in range(0, len(value)):
+        try:
+            key = int(value[i])
+        except:
+            pass
+        value1 = result[key]
+        el = result[id - 1 - key]
+        result[key] = el
+        result[id - 1 - key] = value1
+    a = "".join(result)
+    preCloseCallbackResult = a
+    littleEndian = fn + locaOffset + int(id1)
+
+    def abcd3(littleEndian, preCloseCallbackResult):
+        if littleEndian - 62 < 0:
+            return preCloseCallbackResult[littleEndian]
+        else:
+            p1 = littleEndian % 62
+            p2 = int(littleEndian / 62)
+            return abcd3(p2, preCloseCallbackResult) + preCloseCallbackResult[p1]
+
+    jqparam = abcd3(littleEndian, preCloseCallbackResult)
+    print(jqparam)
+    item = 0
+    map = list(jqparam)
+    for i in range(0, len(jqparam)):
+        item += ord(map[i])
+    el = len(jqparam)
+    selected = item % el
+    el1 = []
+    for i in range(selected, el):
+        el1.append(map[i])
+    for i in range(0, selected):
+        el1.append(map[i])
+    jqparam = "".join(el1)
+    return jqparam
+
+
+ktimes = 37
+jqnonce = '6a4b01e7-13f8-468e-960b-a14a16691646'
+rn = '3525359268.74921822'
+stt = '2021/8/5 22:05:07'
+#'2021-08-05 22:05:07'
+#1628172307000
+#1628172307000
+id1 = '2038256564'
+print(dan(ktimes, jqnonce))
+print(dan(ktimes, '测试'))
+print(get_jqParam(rn, stt, id1))
